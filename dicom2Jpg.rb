c924590af33fa8aab650687d6ee0a1cbf020aa3d
Choi Gunho <ghc0311@naver.com>
2017-01-28 02:34:34 +0900

    Delete dicom2Jpg.rb

--- dicom2Jpg.rb
+++
@@ -1,8 +0,0 @@
-require 'dicom'
-include DICOM
-
-dcm = DObject.read("1.3.12.2.1107.5.1.4.51914.30000006121806122618700000704.dcm")
-dcm_image = dcm.image
-dcm_image.normalize.write("1.3.12.2.1107.5.1.4.51914.30000006121806122618700000704.jpg")
-
-exit
